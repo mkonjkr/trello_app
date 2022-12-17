566863b7823ce5e6c34d336830e94954f6207290
unknown <mkonjkr@gmail.com>
2022-12-17 15:58:16 -0800

    Recoil Selector

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,12 +1,35 @@
-import { atom } from "recoil";
+/* import { atom, selector } from "recoil";
 
-export interface IToDo {
-  text: string;
-  id: number;
-  category: "TO_DO" | "DOING" | "DONE";
-}
+export const minuteState = atom({
+  key: "minutes",
+  default: 0,
+});
 
-export const toDoState = atom<IToDo[]>({
-  key: "toDo",
-  default: [],
+export const hourSelector = selector({
+  key: "hours",
+  get: ({ get }) => {
+    const minutes = get(minuteState);
+    return minutes / 60;
+  },
+});
+
+ */
+
+import { atom, selector } from "recoil";
+
+export const minuteState = atom({
+  key: "minutes",
+  default: 0,
+});
+
+export const hourSelector = selector({
+  key: "hours",
+  get: ({ get }) => {
+    const minutes = get(minuteState);
+    return minutes / 60;
+  },
+  set: ({ set }, newValue) => {
+    const minutes = Number(newValue) * 60;
+    set(minuteState, minutes);
+  },
 });
